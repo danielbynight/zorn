603955b5807f7991152ef0e6acc201db2b2dfc5f
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-12 17:58:38 +0200

    created setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'zorn',
+    version = '0.0.1',
+    author = 'Daniel Matias Ferrer',
+    author_email = 'controlledflame@gmail.com',
+    description = 'A static website generator.',
+    license = "MIT",
+    keywords = 'static website generator',
+    url = 'http://controlledflame.com',
+    packages=['zorn'],
+    long_description=read('README.md'),
+    classifiers=[],
+)
