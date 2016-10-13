c29a5a9f47d7ed3a09241201ba6c3407a74e1f78
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-13 08:25:24 +0200

    cleanup and create example structure

--- example/settings.py
+++ example/settings.py
@@ -1,6 +1,28 @@
-import zorn
+from zorn.Page import Page
 import os
 
+# General
+
+SITE_TITLE = 'Zorn - Getting Started'
+AUTHOR = 'Daniel Matias Ferrer'
+DESCRIPTION = 'A static website generator'
+KEYWORDS = 'static generator website'
+
+# Directories
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
+MARKDOWN_DIR = os.path.join(ROOT_DIR, 'md')
+
+
+# Navigation
+
 PAGES = [
-    zorn
+    Page('Home', 'index'),
+    Page('Documentation', 'doc', [
+        Page('Getting Started', 'getting_started'),
+        Page('CLI', 'cli'),
+        Page('Templates', 'templates'),
+    ]),
+    Page('Contact', 'contact'),
 ]
--- zorn/Page.py
+++ zorn/Page.py
@@ -1,5 +1,7 @@
 class Page:
-    def __init__(self, page_name, file_name, sub_pages=[]):
+    def __init__(self, page_name, file_name, sub_pages=None):
         self.page_name = page_name
         self.file_name = file_name
+        if sub_pages is None:
+            sub_pages = []
         self.sub_pages = sub_pages
