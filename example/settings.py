ff2df05651f1f7cf5061e50fabbf72d45c8924d9
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-13 07:54:38 +0200

    created page

--- example/settings.py
+++ example/settings.py
@@ -1,5 +1,6 @@
+import zorn
 import os
 
 PAGES = [
-
+    zorn
 ]
---
+++ zorn/Page.py
@@ -0,0 +1,5 @@
+class Page:
+    def __init__(self, page_name, file_name, sub_pages=[]):
+        self.page_name = page_name
+        self.file_name = file_name
+        self.sub_pages = sub_pages
