bea5ee653f67a57fc6453097d08106d87ae33e5f
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-16 09:47:07 +0200

    added some more configuration

--- setup.py
+++ setup.py
@@ -11,12 +11,28 @@ setup(
     version='0.0.1',
     author='Daniel Matias Ferrer',
     author_email='controlledflame@gmail.com',
-    description='A static website generator.',
+    description='A static website generator with personality.',
     license='MIT',
     keywords='static website generator',
     url='http://controlledflame.com',
     packages=['zorn'],
+    include_package_data=True,
+    scripts=['bin/zorn'],
+    entry_points={'console_scripts': [
+        'zorn = zorn.cli:process_request',
+    ]},
+    extras_require={
+        'Jinja2': ['Jinja2'],
+        'Markdown': ['Markdown'],
+    },
     long_description=read('README.md'),
-    classifiers=[],
-    scripts=['bin/zorn']
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: JavaScript',
+    ],
 )
