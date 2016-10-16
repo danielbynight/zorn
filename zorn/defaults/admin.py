47b1f2b9b23013c4fbd8a780c502b6c7a8d2560e
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-16 10:33:12 +0200

    added settings path

--- zorn/defaults/admin.py
+++ zorn/defaults/admin.py
@@ -5,5 +5,9 @@ import sys
 from zorn.cli import process_request
 
 if __name__ == '__main__':
-    os.environ.setdefault('ZORN_SETTINGS', '{{ project_name }}.settings')
+    os.environ.setdefault('ZORN_SETTINGS', 'settings')
+    os.environ.setdefault(
+        'ZORN_SETTINGS_PATH',
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),'settings.py')
+    )
     process_request(sys.argv)
