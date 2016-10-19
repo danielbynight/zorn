4b995d2706b41517c1d48ca4b9c648f7803d2efb
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-19 07:48:49 +0200

    only cover source

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=['bin/zorn'],
+    scripts=['zorn/bin/zorn'],
     install_requires=[
         'markdown',
         'Jinja2'
