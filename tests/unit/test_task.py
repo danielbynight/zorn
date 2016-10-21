7a117da86ded1829060c181f899a068f08fcde12
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-21 11:18:56 +0200

    linting

--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -2,15 +2,18 @@ import sys
 from zorn import tasks
 from io import StringIO
 
+
 def test_task():
     task = tasks.Task()
     assert task.verbosity == 1
+
 
 def test_parse_verbosity_standard():
     silent = False
     verbose = False
     verbosity = tasks.Task.parse_verbosity(verbose, silent)
     assert verbosity == 1
+
 
 def test_parse_verbosity_silent():
     silent = True
@@ -22,11 +25,13 @@ def test_parse_verbosity_silent():
     verbosity = tasks.Task.parse_verbosity(verbose, silent)
     assert verbosity == 0
 
+
 def test_parse_verbosity_verbose():
     silent = False
     verbose = True
     verbosity = tasks.Task.parse_verbosity(verbose, silent)
     assert verbosity == 2
+
 
 def test_comunicate_standard_verbosity():
     task = tasks.Task(1)
@@ -38,6 +43,7 @@ def test_comunicate_standard_verbosity():
     sys.stdout = stdout_
     assert stream.getvalue() == 'standard\n'
 
+
 def test_comunicate_silent():
     task = tasks.Task(0)
     stdout_ = sys.stdout
@@ -47,6 +53,7 @@ def test_comunicate_silent():
     task.communicate('verbose', False)
     sys.stdout = stdout_
     assert stream.getvalue() == ''
+
 
 def test_comunicate_verbose():
     task = tasks.Task(2)
