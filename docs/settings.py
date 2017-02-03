b3fcd32f780f0b84b3716ef9606324c4b5f01013
Daniel Matias Ferrer <xassbit@gmail.com>
2017-02-03 08:19:55 +0100

    correct permissions

--- docs/settings.py
+++ docs/settings.py
@@ -7,7 +7,8 @@ DEBUG = True
 # General
 
 PROJECT_NAME = 'docs'
-SITE_TITLE = 'Zorn Package Documentation'
+SITE_TITLE = 'Zorn'
+SITE_SUBTITLE = 'A static site generator'
 AUTHOR = 'danielmatiasferrer'
 
 # Directories
@@ -16,6 +17,15 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Navigation
 
+URL_STYLE = 'nested'
+
 PAGES = [
     elements.Page('Home', 'index'),
+    elements.Page('Documentation', 'documentation', [
+        elements.SubPage('Command line', 'cli'),
+        elements.SubPage('Routing', 'routing'),
+        elements.SubPage('Project structure', 'structure'),
+        elements.SubPage('Style', 'style'),
+        elements.SubPage('Automation', 'automation')
+    ])
 ]
