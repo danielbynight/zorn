dd0f1ca3c71f6c787d2bff01ff301d55b6475e09
xassbit <xassbit@gmail.com>
2017-10-14 14:22:53 +0200

    increase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='zorn',
-    version='1.0.1',
+    version='1.0.2',
     author='Daniel Matias Ferrer',
     author_email='controlledflame@gmail.com',
     description='A light static website generator.',
